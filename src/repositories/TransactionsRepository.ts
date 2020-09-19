39d4bfebcc06b70c2640117554fc8d62ce8455c9
Artur Rafael da Silveira <arturrsilveira85@gmail.com>
2020-09-18 21:31:40 -0300

    Finalizado criação de transações

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,4 +1,6 @@
-import { EntityRepository, Repository } from 'typeorm';
+/* eslint-disable no-param-reassign */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { EntityRepository, Repository, TableInheritance } from 'typeorm';
 
 import Transaction from '../models/Transaction';
 
@@ -11,7 +13,29 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const { income, outcome } = transactions.reduce(
+      (incrementValue, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            incrementValue.income += Number(transaction.value);
+            break;
+          case 'outcome':
+            incrementValue.outcome += Number(transaction.value);
+            break;
+          default:
+            break;
+        }
+        return incrementValue;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 }
 
